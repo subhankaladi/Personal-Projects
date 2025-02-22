b1fc7b0ae543104bd9d8ef0093fa27ef8c779464
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-02-22 15:08:12 +0500

    Set Configuration

--- Python-Quiz-App/for-youtube/next.config.ts
+++ Python-Quiz-App/for-youtube/next.config.ts
@@ -1,8 +1,22 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     domains: ["cdn.sanity.io"], // Allow images from Sanity
+//   },
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     domains: ["cdn.sanity.io"], // Allow images from Sanity
+  },
+  env: {
+    SANITY_API_TOKEN: process.env.SANITY_API_TOKEN, // Add this line
   },
 };
 
