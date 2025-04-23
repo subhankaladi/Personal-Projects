e5e3eeee32a580adab89a521cf79a356816dc74b
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-04-24 04:12:58 +0500

    Update client.ts

--- Python-Quiz-App/for-youtube/sanity/lib/client.ts
+++ Python-Quiz-App/for-youtube/sanity/lib/client.ts
@@ -6,6 +6,6 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  token: process.env.SANITY_API_TOKEN || "sk7JNQ6Zdruc0ZDuTr5N6QBZVMQvIcUfXSvmCVLzuoZe5zfrVW6nLzoMZmTu62VCaleTLZyQe08yTCKFCb1dTn8qsU0BpBGY5UQQh3nZf3C0XfMKJZCNwEPGeghL22pbnlKCNMqKmsPEjNoqr2g41qgnvhCBTZ9vrv1uwPJcLL4wSt8Rm9Se",
-  useCdn: true // Set to false if statically generating pages, using ISR or tag-based revalidation
+  token: process.env.SANITY_API_TOKEN,
+  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
